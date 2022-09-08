7e766348168e335da6e741a0f6c18d5f1272a35f
HassanAdemuyiwa <ademuyiwahassan68@gmail.com>
2022-09-08 14:48:34 -0700

    ...

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Programmin is like bulding a multilingual 
+ * 		 puzzle, floowed by new line.
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	puts("\"PRogrammingis like building a multilingual puzzle");
+
+	return (0);
+}
