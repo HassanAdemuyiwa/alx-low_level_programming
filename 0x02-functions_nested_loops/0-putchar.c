81aef2821afbf7152888b25549ae85ef6439477f
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2022-09-13 22:03:13 +0100

    ...

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * main - Prints out _putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(char c)
+	return (0);
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_
+/*
+ * header - This is header file
+ *
+ * Return: Always 0.
+ */
+
+int _putchar(char c);
+
+#endif
