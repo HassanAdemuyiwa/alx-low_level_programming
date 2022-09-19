83917e9914410aac20803a3ceb18774359997ec8
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2022-09-19 22:53:38 +0100

    ...

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,11 @@
  *
  * Return: The length of @str.
  */
-int _strlen(const char *s)
+int _strlen(char *str)
 {
 	int length = 0;
 
-	while (*s++)
+	while (*str++)
 		length++;
 	return (length);
 }
