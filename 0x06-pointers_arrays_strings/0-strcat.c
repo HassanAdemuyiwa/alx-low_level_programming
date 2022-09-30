c1b044a56e046adffbe7f4b44542e74c0d49d331
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2022-09-30 13:19:49 +0100

    ...

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates two strings
+ * _strcat - Concatenates two strings
  * @dest: A pointer to the string to be concatenated
  * @src: The source string to be appended to @dest.
  *
