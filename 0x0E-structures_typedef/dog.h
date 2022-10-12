7ea746772a4e2dd3c3754d7a144143b0b752e2ff
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2022-10-12 12:29:38 +0100

    added description to dog struct file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * Desc: This is a dog struct header file
+ */
+
+/**
+ * struct dog - A new type dog
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
