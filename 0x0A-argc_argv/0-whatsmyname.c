3e8a5b1cf09ca5ae3953f7109b62f91562779f11
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2023-10-03 13:21:37 +0100

    first task in argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: count of argument passed
+ * @argv: pointer of array of string passed to
+ *
+ * Return: Alway 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
